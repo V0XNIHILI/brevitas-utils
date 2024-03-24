740d46ddcc22b777a1dcd75ec65c19335e720b8c
Douwe den Blanken <douwedb@gmail.com>
2024-03-24 20:24:09 +0100

    Make sure single-valued scales and zero-points are output as floats

--- brevitas_utils/export.py
+++ brevitas_utils/export.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 
 import brevitas.nn as qnn
 
-def get_quant_weights_and_biases(quant_model: nn.Module, input_shape: tuple):
+
+def get_quant_weights_and_biases(quant_model: nn.Module, input_shape: tuple, as_numpy: bool = False):
     layers = {}
 
     for name, module in quant_model.named_modules():
@@ -14,9 +15,22 @@ def get_quant_weights_and_biases(quant_model: nn.Module, input_shape: tuple):
 
     for name, module in quant_model.named_modules():
         if name.endswith('act_quant'):
+            scale = module.scale().cpu().detach()
+            zero_point = module.zero_point().cpu().detach()
+
+            if as_numpy:
+                scale = scale.numpy()
+                zero_point = zero_point.numpy()
+
+            if len(scale.shape) == 0:
+                scale = scale.item()
+
+            if len(zero_point.shape) == 0:
+                zero_point = zero_point.item()
+
             activations[name] = {
-                'scale': module.scale().cpu().detach(),
-                'zero_point': module.zero_point().cpu().detach()
+                'scale': scale,
+                'zero_point': zero_point
             }
 
     # Make sure bias can be accessed
@@ -28,6 +42,8 @@ def get_quant_weights_and_biases(quant_model: nn.Module, input_shape: tuple):
 
     parameters = {}
 
+    # TODO: add input and output quant scale and zero-point
+
     for name, layer in layers.items():
         quant_weight = layer.quant_weight().value.cpu().detach()
         quant_bias = layer.quant_bias()
@@ -35,7 +51,17 @@ def get_quant_weights_and_biases(quant_model: nn.Module, input_shape: tuple):
         if quant_bias is not None:
             quant_bias = quant_bias.value.cpu().detach()
 
+            if as_numpy:
+                quant_bias = quant_bias.numpy()
+
         scale = layer.quant_weight().scale.cpu().detach()
+
+        if as_numpy:
+            quant_weight = quant_weight.numpy()
+            scale = scale.numpy()
+
+        if len(scale.shape) == 0:
+            scale = scale.item()
 
         parameters[name] = {
             'weight': quant_weight,
