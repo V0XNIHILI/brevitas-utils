89514d67a0c7e731a4f755ec7b1095eb5e232d9c
Douwe den Blanken <douwedb@gmail.com>
2024-03-27 22:09:59 +0100

    Check if scaling point implementation is ZeroZeroPoint and use jit.script for quantize function

--- brevitas_utils/custom_quantizers/Int8WeightPerTensorPowerOfTwo.py
+++ brevitas_utils/custom_quantizers/Int8WeightPerTensorPowerOfTwo.py
@@ -1,11 +1,37 @@
+"""Complete approach based on: https://github.com/Xilinx/brevitas/blob/master/notebooks/03_anatomy_of_a_quantizer.ipynb
+"""
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 import brevitas
 
 from brevitas.core.quant.delay import DelayWrapper
 from brevitas.proxy import WeightQuantProxyFromInjector
 from brevitas.quant.fixed_point import Int8WeightPerTensorFixedPoint
+from brevitas.core.zero_point import ZeroZeroPoint
+
+
+@torch.jit.script
+def clamped_quantize_power_of_two(input: torch.Tensor, bit_width: int):
+    sign = input.sign()
+    input_abs = input.abs()
+
+    input_clamped = input_abs.clamp(min=-1, max=1)
+
+    minimum_log2_value = int(-(2**(bit_width-1) - 1))
+    maximum_log2_value = 0
+
+    rounded_log2_values = torch.round(torch.log2(input_clamped))
+    clamped_rounded_log2_values = torch.clamp(rounded_log2_values,
+                                                minimum_log2_value,
+                                                maximum_log2_value)
+
+    # As sign is 0 or 1, we need to convert it to -1 or 1
+    zero_corrected_sign = (sign + 1.0).sign() * 2.0 - 1.0
+    
+    return torch.exp2(clamped_rounded_log2_values) * zero_corrected_sign
 
 
 class ClampedQuantizePowerOfTwo(torch.autograd.Function):
@@ -14,29 +40,15 @@ class ClampedQuantizePowerOfTwo(torch.autograd.Function):
 
     @staticmethod
     def forward(_, input: torch.Tensor, bit_width: int):
-        sign = input.sign()
-        input_abs = input.abs()
-
-        input_clamped = input_abs.clamp(min=-1, max=1)
-
-        minimum_log2_value: int = -(2**(bit_width-1) - 1)
-        maximum_log2_value = 0
-
-        rounded_log2_values = torch.round(torch.log2(input_clamped))
-        clamped_rounded_log2_values = torch.clamp(rounded_log2_values,
-                                                  minimum_log2_value,
-                                                  maximum_log2_value)
-
-        # As sign is 0 or 1, we need to convert it to -1 or 1
-        zero_corrected_sign = (sign + 1.0).sign() * 2.0 - 1.0
-        
-        return torch.exp2(clamped_rounded_log2_values) * zero_corrected_sign
+        return clamped_quantize_power_of_two(input, bit_width)
 
     @staticmethod
     def backward(_, grad_output: torch.Tensor):
         return grad_output, None
 
+
 potquant = ClampedQuantizePowerOfTwo.apply
+
 
 class ClampedPoTQuantizer(brevitas.jit.ScriptModule):
 
@@ -47,6 +59,7 @@ class ClampedPoTQuantizer(brevitas.jit.ScriptModule):
                  signed,
                  quant_delay_steps: int = 0):
         super(ClampedPoTQuantizer, self).__init__()
+
         self.scaling_impl = scaling_impl
         self.bit_width = bit_width
         self.zero_point_impl = zero_point_impl
@@ -61,7 +74,11 @@ class ClampedPoTQuantizer(brevitas.jit.ScriptModule):
     @brevitas.jit.script_method
     def forward(self, x: torch.Tensor):
         scale = self.scaling_impl(x)
-        # TODO: check if zero-point is ZeroZeroPointImpl
+
+        # Check if zero-point is ZeroZeroPointImpl, else raise error
+        if not isinstance(self.zero_point_impl, ZeroZeroPoint):
+            raise NotImplementedError("Zero-point must be ZeroZeroPointImpl")
+
         zero_point = self.zero_point_impl(x, scale, self.bit_width) # TODO: not sure if passing this bitwidth is correct
 
         y = potquant(x / scale, int(self.bit_width)) * scale
