1e0862ad213f83d8409a12f3e0f5fd27f415860a
Douwe den Blanken <douwedb@gmail.com>
2025-11-10 09:27:16 +0100

    Make ModWithClampGrad work again for raw audio KWS and add support for custom gradient outside clamp range

--- brevitas_utils/custom_clamps/ignore_overflow_clamp.py
+++ brevitas_utils/custom_clamps/ignore_overflow_clamp.py
@@ -6,31 +6,42 @@ import brevitas
 
 class ModWithClampGrad(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, x: Tensor, min_val: Tensor, max_val: Tensor):
-        ctx.save_for_backward(min_val, max_val, x)
-        
+    def forward(ctx, x: Tensor, min_val: Tensor, max_val: Tensor, gradient_outside_range: float = 1.0):
         range_val = max_val - min_val + 1
         wrapped = (x - min_val) % range_val + min_val
-        
+
+        ctx.save_for_backward(min_val, max_val, x)
+        ctx.gradient_outside_range = gradient_outside_range
+
         return wrapped
 
     @staticmethod
     def backward(ctx, grad_output):
         min_val, max_val, x = ctx.saved_tensors
+        gradient_outside_range = ctx.gradient_outside_range
 
+        if gradient_outside_range == 1.0:
+            return grad_output, None, None, None
+        
         grad_input = grad_output.clone()
 
-        grad_input[x < min_val] = 0
-        grad_input[x > max_val] = 0
+        if gradient_outside_range == 0.0:
+            grad_input[x < min_val] = 0
+            grad_input[x > max_val] = 0
 
-        return grad_input, None, None
+            return grad_input, None, None, None
+        
+        grad_input[x < min_val] = gradient_outside_range * grad_output[x < min_val]
+        grad_input[x > max_val] = -gradient_outside_range * grad_output[x > max_val]
+
+        return grad_input, None, None, None
 
 
 mod_with_clamp_grad_func = ModWithClampGrad.apply
 
 
 @brevitas.jit.script
-def ignore_overflow_clamp(x: Tensor, min_val: Tensor, max_val: Tensor, zero_gradients_outside_range: bool = False) -> Tensor:
+def ignore_overflow_clamp(x: Tensor, min_val: Tensor, max_val: Tensor, gradient_outside_range: float = 1.0) -> Tensor:
     """
     Generalized overflow function that wraps values exceeding the specified range.
 
@@ -38,7 +49,8 @@ def ignore_overflow_clamp(x: Tensor, min_val: Tensor, max_val: Tensor, zero_grad
         x: Input tensor on which to apply the overflow behavior.
         min_val: Minimum values for the range.
         max_val: Maximum values for the range.
-        zero_gradients_outside_range: If True, gradients are zeroed outside the range. This can help with convergence, especially for really deep networks.
+        zero_gradients_outside_range: If set to 0, gradients are zeroed outside the range. This can help with convergence, especially for really deep networks.
+            If set to 1, gradients are passed through unchanged. If set to -1.0, the gradients look like this: [-1 (before min), 1 (inside range), 2 (after max)].
 
     Notes:
         x, min_val, max_val need to be broadcastable.
@@ -52,12 +64,7 @@ def ignore_overflow_clamp(x: Tensor, min_val: Tensor, max_val: Tensor, zero_grad
         tensor([0.7000, 0.5000, 0.1000])
     """
 
-    if zero_gradients_outside_range:
-        return mod_with_clamp_grad_func(x, min_val, max_val)
-    else:
-        range_val = max_val - min_val + 1
-        wrapped = (x - min_val) % range_val + min_val
-        return wrapped
+    return mod_with_clamp_grad_func(x, min_val, max_val, gradient_outside_range)
 
 
 class IgnoreOverflowClamp(brevitas.jit.ScriptModule):
@@ -72,11 +79,13 @@ class IgnoreOverflowClamp(brevitas.jit.ScriptModule):
         tensor([1.,  0.])
     """
 
-    def __init__(self, zero_gradients_outside_range: bool = False):
+    def __init__(self, gradient_outside_range: float = 1.0):
         super(IgnoreOverflowClamp, self).__init__()
 
-        self.zero_gradients_outside_range = zero_gradients_outside_range
+        assert gradient_outside_range <= 1.0, "Only gradient_outside_range <= 1.0 is supported."
+
+        self.gradient_outside_range = gradient_outside_range
 
     @brevitas.jit.script_method
     def forward(self, x: Tensor, min_val: Tensor, max_val: Tensor):
-        return ignore_overflow_clamp(x, min_val=min_val, max_val=max_val, zero_gradients_outside_range=self.zero_gradients_outside_range)
+        return ignore_overflow_clamp(x, min_val=min_val, max_val=max_val, gradient_outside_range=self.gradient_outside_range)
